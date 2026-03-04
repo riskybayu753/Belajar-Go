ec0880836f30668644dc6556e826781c131e3550
ILHAM BAYU R.N.A <79826253+ilhambayu001@users.noreply.github.com>
2026-03-04 13:45:54 +0700

    belajar tipe data, disini aku bingung kenapa harus menggunakan %d ?

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var firstName string = "ilham"
+	var positiveNumber uint8 = 89
+	var negativeNumber = -1243423644
 
-	var LastName string
-	LastName = "bayu"
+	fmt.Print("bilangan posifi: %d\n", positiveNumber)
+	fmt.Print("bilangan negatif: %d\n", negativeNumber)
 
-	fmt.Printf("halo %s %s!\n", firstName, LastName)
 }
