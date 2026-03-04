712a14f88172ad8b5a08120a3d0c4ebb65c1aa66
ILHAM BAYU R.N.A <79826253+ilhambayu001@users.noreply.github.com>
2026-03-04 13:34:26 +0700

    belajar variabel var

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
-	fmt.Println("hai nama saya ilham")
+	var firstName string = "ilham"
+
+	var LastName string
+	LastName = "bayu"
+
+	fmt.Printf("halo %s %s!\n", firstName, LastName)
 }
